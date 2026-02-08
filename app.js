52888c104bcd0655ed6fa337a02827736b30a339
SuperexMack <unilexerox@gmail.com>
2026-02-08 21:14:53 +0530

    Done with the dynamic movement

--- app.js
+++ app.js
@@ -1,32 +1,27 @@
-import express from "express"
-import ffmeg from "ffmpeg"
-const app = express()
-const PORT = 9000
+import { exec } from "child_process";
+import path from "path";
 
-app.get("/",(req,res)=>{
-	return res.json({msg:"This is the ffmeg"})
+// const output = path.resolve((`${new Date().toLocaleString().split(",")[1]}.mp4`))
+const inputFile = path.resolve("./Content/addingwatermark.mp4")
 
-})
+function makeDynamicWatermark() {
+  const command = `
+    ffmpeg -i ${inputFile} \
+-vf \
+"drawtext=fontfile=font.ttf:fontsize=80:fontcolor=red@0.5:text=userid1345: \
+ x=if(eq(mod(t\\,2)\\,0)\\,rand(0\\,(W-tw))\\,x): \
+ y=if(eq(mod(t\\,3)\\,0)\\,rand(0\\,(H-th))\\,y)" \
+-c:v libx264 -crf 1 -c:a copy addingwatermarkoutput.mp4
+`;
 
-const checkVideo = ()=>{
-  let process = new ffmeg('./fa.mp4');
-  process
-  .then((video)=>{
-	  video.fnAddWatermark("./oneoo.jpg" , "./onePeice.mp4",{
-		  position:"SE",
-	  },(error,file)=>{
-		  if(!error) console.log("Your file is " + file)
-		  else console.log("Some error occured while giving the file")
-	  })
-  })
-  .catch((error)=>{
-    console.log("something went wrong while processing the data" + error)
-  })
+  exec(command, (error, stderr) => {
+    if (error) {
+      console.error("FFmpeg ERROR");
+      console.error(stderr);
+      return;
+    }
+    console.log("Dynamic watermark added");
+  });
 }
 
-
-checkVideo()
-
-app.listen(PORT , ()=>{
-	console.log(`Server is running on the port number ${PORT}`)
-})
+makeDynamicWatermark();
