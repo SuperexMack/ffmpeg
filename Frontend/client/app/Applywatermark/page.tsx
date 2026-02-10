2cef9b110312e9e4069a4819ac2116fcf864409d
SuperexMack <unilexerox@gmail.com>
2026-02-10 12:13:41 +0530

    Added backend link to the frontend

--- Frontend/client/app/Applywatermark/page.tsx
+++ Frontend/client/app/Applywatermark/page.tsx
@@ -25,7 +25,7 @@ export default function AddWaterMark() {
     const formData = new FormData()
     formData.append("file",file)
 
-    let res = await fetch("http://localhost:9000/getvideo/sendVideo",{
+    let res = await fetch("https://savebiss.onrender.com/getvideo/sendVideo",{
         method:"POST",
         body:formData
     })
