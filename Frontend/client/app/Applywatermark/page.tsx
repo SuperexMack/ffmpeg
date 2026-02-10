b60812e92bd58b3b0b8ed8e20a822d0ecf8e1250
SuperexMack <unilexerox@gmail.com>
2026-02-10 23:12:55 +0530

    Added Frontend

--- Frontend/client/app/Applywatermark/page.tsx
+++ Frontend/client/app/Applywatermark/page.tsx
@@ -27,7 +27,7 @@ export default function AddWaterMark() {
     setLoading(true);
     const formData = new FormData();
     formData.append("file", file);
-    let res = await fetch("http://localhost:9000/getvideo/sendVideo", {
+    let res = await fetch("https://ffmpeg-production-1b52.up.railway.app/getvideo/sendVideo", {
       method: "POST",
       body: formData,
     });
@@ -48,7 +48,7 @@ export default function AddWaterMark() {
     if (!userId) return;
     const interval = setInterval(async () => {
       try {
-        const response = await fetch(`http://localhost:9000/pooling/${userId}`);
+        const response = await fetch(`https://ffmpeg-production-1b52.up.railway.app/pooling/${userId}`);
         if (response.status === 202){
           toast.info("Processing")
           return
