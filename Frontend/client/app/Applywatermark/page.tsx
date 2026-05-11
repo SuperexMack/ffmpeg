1e69fec1ac6445aac15ac9ac3f4f7271c714fc88
SuperexMack <unilexerox@gmail.com>
2026-05-11 17:13:59 +0530

    Fixe the routes

--- Frontend/client/app/Applywatermark/page.tsx
+++ Frontend/client/app/Applywatermark/page.tsx
@@ -27,7 +27,7 @@ export default function AddWaterMark() {
     formData.append("file", file);
     // The free servers were not working so i used localLink but we are working on it will fix it soon
     let res = await fetch(
-      "https://api.savebiss.mohitsati.page:9000/getvideo/sendVideo",
+      "https://api.savebiss.mohitsati.page/getvideo/sendVideo",
       {
         method: "POST",
         body: formData,
@@ -52,7 +52,7 @@ export default function AddWaterMark() {
       try {
         // The free servers were not working so i used localLink but we are working on it will fix it soon
         const response = await fetch(
-          `https://api.savebiss.mohitsati.page:9000/pooling/${userId}`,
+          `https://api.savebiss.mohitsati.page/pooling/${userId}`,
         );
         if (response.status === 202) {
           toast.info("Processing");
